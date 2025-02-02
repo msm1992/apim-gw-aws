9abcd417a8784d739db23a91e86c5d663f631b05
msm1992 <madusha.sachini@yahoo.com>
2025-02-02 10:10:32 +0530

    Implement redeploy and undeploy, refactor the codebase to handle exceptions, introduce a Multiton AWS client manager to maintain gateway clients in a reusable manner

--- components/aws.gw.manager/src/main/java/org/wso2/aws/client/util/AWSAPIUtil.java
+++ components/aws.gw.manager/src/main/java/org/wso2/aws/client/util/AWSAPIUtil.java
@@ -1,18 +1,37 @@
 package org.wso2.aws.client.util;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.model.API;
+import org.wso2.carbon.apimgt.api.model.Environment;
+import org.wso2.carbon.apimgt.impl.deployer.exceptions.DeployerException;
+import org.wso2.carbon.apimgt.impl.utils.APIUtil;
 import software.amazon.awssdk.core.SdkBytes;
-import software.amazon.awssdk.http.SdkHttpClient;
-import software.amazon.awssdk.http.apache.ApacheHttpClient;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient;
-import software.amazon.awssdk.services.apigateway.model.*;
+import software.amazon.awssdk.services.apigateway.model.CreateDeploymentRequest;
+import software.amazon.awssdk.services.apigateway.model.CreateDeploymentResponse;
+import software.amazon.awssdk.services.apigateway.model.DeleteDeploymentRequest;
+import software.amazon.awssdk.services.apigateway.model.DeleteStageRequest;
+import software.amazon.awssdk.services.apigateway.model.Deployment;
+import software.amazon.awssdk.services.apigateway.model.GetDeploymentsRequest;
+import software.amazon.awssdk.services.apigateway.model.GetDeploymentsResponse;
+import software.amazon.awssdk.services.apigateway.model.GetResourcesRequest;
+import software.amazon.awssdk.services.apigateway.model.GetResourcesResponse;
+import software.amazon.awssdk.services.apigateway.model.ImportRestApiRequest;
+import software.amazon.awssdk.services.apigateway.model.ImportRestApiResponse;
+import software.amazon.awssdk.services.apigateway.model.IntegrationType;
+import software.amazon.awssdk.services.apigateway.model.Method;
+import software.amazon.awssdk.services.apigateway.model.PutIntegrationRequest;
+import software.amazon.awssdk.services.apigateway.model.PutIntegrationResponse;
+import software.amazon.awssdk.services.apigateway.model.PutIntegrationResponseRequest;
+import software.amazon.awssdk.services.apigateway.model.PutMode;
+import software.amazon.awssdk.services.apigateway.model.PutRestApiRequest;
+import software.amazon.awssdk.services.apigateway.model.PutRestApiResponse;
+import software.amazon.awssdk.services.apigateway.model.Resource;
 
 import java.util.List;
 import java.util.Map;
@@ -20,13 +39,20 @@ import java.util.Map;
 public class AWSAPIUtil {
     private static final Log log = LogFactory.getLog(AWSAPIUtil.class);
 
-    public static String importRestAPI (API api) {
-        String openAPI = api.getSwaggerDefinition();
-        String apiId = null;
-        Region region = Region.EU_NORTH_1;
-        SdkHttpClient httpClient = ApacheHttpClient.builder().build();
+    private static final String REGION = "eu-north-1";
+    private static final String ACCESS_KEY = "AKIA2MNVLZRSO7M7GI4Y";
+    private static final String SECRET_ACCESS_KEY = "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";
 
-        try (ApiGatewayClient apiGatewayClient = ApiGatewayClient.builder().httpClient(httpClient).region(region).build()) {
+
+    public static String importRestAPI (API api, Environment environment) throws DeployerException {
+        String openAPI = api.getSwaggerDefinition();
+        ApiGatewayClient apiGatewayClient = null;
+        String apiId = null;
+
+        try {
+            apiGatewayClient = ApiGatewayClientManager
+                    .getClient(REGION, ACCESS_KEY, SECRET_ACCESS_KEY);
+
             ImportRestApiRequest importApiRequest = ImportRestApiRequest.builder()
                     .body(SdkBytes.fromUtf8String(openAPI))
                     .failOnWarnings(false)
@@ -65,11 +91,13 @@ public class AWSAPIUtil {
                                 .type(IntegrationType.HTTP)
                                 .uri(productionEndpoint + resource.path())
                                 .build();
-                        PutIntegrationResponse putIntegrationResponse = apiGatewayClient.putIntegration(putIntegrationRequest);
+                        PutIntegrationResponse putIntegrationResponse =
+                                apiGatewayClient.putIntegration(putIntegrationRequest);
                         String integrationURI = putIntegrationResponse.uri();
 
                         //Configure default output mapping
-                        PutIntegrationResponseRequest putIntegrationResponseRequest = PutIntegrationResponseRequest.builder()
+                        PutIntegrationResponseRequest putIntegrationResponseRequest =
+                                PutIntegrationResponseRequest.builder()
                                 .httpMethod(entry.getKey().toString())
                                 .resourceId(resource.id())
                                 .restApiId(apiId)
@@ -81,20 +109,146 @@ public class AWSAPIUtil {
                 }
             }
 
-            //create stage and deploy API
-            CreateDeploymentRequest createDeploymentRequest = CreateDeploymentRequest.builder().restApiId(apiId)
-                    .stageDescription("Dev Stage").build();
-            CreateDeploymentResponse createDeploymentResponse = apiGatewayClient.createDeployment(createDeploymentRequest);
-            String deploymentId = createDeploymentResponse.id();
+            String stageName = GatewatUtil.getStageOfGatewayEnvironment(environment.getGatewayType());
 
-            if (StringUtils.isNotEmpty(deploymentId)) {
-                CreateStageRequest createStageRequest = CreateStageRequest.builder().restApiId(apiId).stageName("dev")
-                        .deploymentId(deploymentId).build();
-                apiGatewayClient.createStage(createStageRequest);
-            }
+            CreateDeploymentRequest createDeploymentRequest = CreateDeploymentRequest.builder().restApiId(apiId)
+                    .stageName(stageName).build();
+            apiGatewayClient.createDeployment(createDeploymentRequest);
         } catch (Exception e) {
-            log.error("Error occurred while importing API: " + e.getMessage());
+            try {
+                GatewatUtil.rollbackDeployment(apiGatewayClient, apiId, api.getUuid(), environment.getUuid());
+            } catch (APIManagementException ex) {
+                throw new DeployerException("Error occurred while rolling back deployment: " + ex.getMessage());
+            }
+            throw new DeployerException("Error occurred while importing API: " + e.getMessage());
         }
         return apiId;
+    }
+
+    public static void reimportRestAPI(String awsApiId, API api, Environment environment) throws DeployerException {
+
+        ApiGatewayClient apiGatewayClient = null;
+        try {
+            String openAPI = api.getSwaggerDefinition();
+
+            apiGatewayClient = ApiGatewayClientManager.getClient(REGION, ACCESS_KEY, SECRET_ACCESS_KEY);
+            PutRestApiRequest reimportApiRequest = PutRestApiRequest.builder()
+                    .restApiId(awsApiId)
+                    .body(SdkBytes.fromUtf8String(openAPI))
+                    .failOnWarnings(false)
+                    .mode(PutMode.OVERWRITE)
+                    .build();
+            PutRestApiResponse reimportApiResponse = apiGatewayClient.putRestApi(reimportApiRequest);
+
+            awsApiId = reimportApiResponse.id();
+
+            //add integrations for each resource
+            GetResourcesRequest getResourcesRequest = GetResourcesRequest.builder()
+                    .restApiId(awsApiId)
+                    .build();
+            GetResourcesResponse getResourcesResponse = apiGatewayClient.getResources(getResourcesRequest);
+
+            //todo: extract endpoint from the API's endpoint configuration
+            String endpointConfig = api.getEndpointConfig();
+            JSONParser parser = new JSONParser();
+            JSONObject endpointConfigJson = (JSONObject) parser.parse(endpointConfig);
+            JSONObject prodEndpoints = (JSONObject)endpointConfigJson.get("production_endpoints");
+            String productionEndpoint = (String) prodEndpoints.get("url");
+
+            productionEndpoint = productionEndpoint.charAt(productionEndpoint.length() - 1) == '/' ?
+                    productionEndpoint.substring(0, productionEndpoint.length() - 1) : productionEndpoint;
+
+            List<Resource> resources = getResourcesResponse.items();
+            for (Resource resource : resources) {
+                Map<String, Method> resourceMethods = resource.resourceMethods();
+                if (!resourceMethods.isEmpty()) {
+                    for (Map.Entry entry : resourceMethods.entrySet()) {
+                        PutIntegrationRequest putIntegrationRequest = PutIntegrationRequest.builder()
+                                .httpMethod(entry.getKey().toString())
+                                .integrationHttpMethod(entry.getKey().toString())
+                                .resourceId(resource.id())
+                                .restApiId(awsApiId)
+                                .type(IntegrationType.HTTP)
+                                .uri(productionEndpoint + resource.path())
+                                .build();
+                        apiGatewayClient.putIntegration(putIntegrationRequest);
+
+                        //Configure default output mapping
+                        PutIntegrationResponseRequest putIntegrationResponseRequest =
+                                PutIntegrationResponseRequest.builder()
+                                        .httpMethod(entry.getKey().toString())
+                                        .resourceId(resource.id())
+                                        .restApiId(awsApiId)
+                                        .statusCode("200")
+                                        .responseTemplates(Map.of("application/json", ""))
+                                        .build();
+                        apiGatewayClient.putIntegrationResponse(putIntegrationResponseRequest);
+                    }
+                }
+            }
+
+            String stageName = GatewatUtil.getStageOfGatewayEnvironment(environment.getGatewayType());
+
+            // deploy API
+            CreateDeploymentRequest createDeploymentRequest = CreateDeploymentRequest.builder().restApiId(awsApiId)
+                    .stageName(stageName).build();
+            CreateDeploymentResponse createDeploymentResponse =
+                    apiGatewayClient.createDeployment(createDeploymentRequest);
+            String deploymentId = createDeploymentResponse.id();
+
+            GetDeploymentsRequest getDeploymentsRequest = GetDeploymentsRequest.builder().restApiId(awsApiId).build();
+            GetDeploymentsResponse getDeploymentsResponse = apiGatewayClient.getDeployments(getDeploymentsRequest);
+            List<Deployment> deployments = getDeploymentsResponse.items();
+            for (Deployment deployment : deployments) {
+                if (!deployment.id().equals(deploymentId)) {
+                    DeleteDeploymentRequest deleteDeploymentRequest = DeleteDeploymentRequest.builder()
+                            .deploymentId(deployment.id())
+                            .restApiId(awsApiId)
+                            .build();
+                    apiGatewayClient.deleteDeployment(deleteDeploymentRequest);
+                }
+            }
+        } catch (Exception e) {
+            try {
+                GatewatUtil.rollbackDeployment(apiGatewayClient, awsApiId, api.getUuid(), environment.getUuid());
+            } catch (APIManagementException ex) {
+                throw new DeployerException("Error occurred while rolling back deployment: " + ex.getMessage());
+            }
+            throw new DeployerException("Error occurred while re-importing API: " + e.getMessage());
+        }
+    }
+
+    public static boolean deleteDeployment(String apiId, Environment environment) throws DeployerException {
+        try {
+            String awsApiId = APIUtil.getApiAWSApiMappingByApiId(apiId, environment.getUuid());
+            if (awsApiId == null) {
+                throw new DeployerException("API ID is not mapped with AWS API ID");
+            }
+
+            ApiGatewayClient apiGatewayClient = ApiGatewayClientManager
+                    .getClient(REGION, ACCESS_KEY, SECRET_ACCESS_KEY);
+            String stageName = GatewatUtil.getStageOfGatewayEnvironment(environment.getGatewayType());
+
+            // Delete the stage before deleting the deployment
+            DeleteStageRequest deleteStageRequest = DeleteStageRequest.builder()
+                    .restApiId(awsApiId)
+                    .stageName(stageName)
+                    .build();
+            apiGatewayClient.deleteStage(deleteStageRequest);
+
+            GetDeploymentsRequest getDeploymentsRequest = GetDeploymentsRequest.builder().restApiId(awsApiId).build();
+            GetDeploymentsResponse getDeploymentsResponse = apiGatewayClient.getDeployments(getDeploymentsRequest);
+            List<Deployment> deployments = getDeploymentsResponse.items();
+            for (Deployment deployment : deployments) {
+                DeleteDeploymentRequest deleteDeploymentRequest = DeleteDeploymentRequest.builder()
+                        .deploymentId(deployment.id())
+                        .restApiId(awsApiId)
+                        .build();
+                apiGatewayClient.deleteDeployment(deleteDeploymentRequest);
+            }
+            return true;
+        } catch (APIManagementException e) {
+            throw new DeployerException("Error occurred while deleting deployment: " + e.getMessage());
+        }
     }
 }
